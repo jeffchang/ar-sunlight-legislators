1add2a9bdf8b9e46b21c34e3d97185f8b98c5bb7
dbc-apprentice <dbc@dbc.com>
2013-08-01 11:21:38 -0700

    Data imported

---
+++ db/migrate/20130801095000_create_legislators.rb
@@ -0,0 +1,24 @@
+require_relative '../../app/models/legislator'
+
+class CreateLegislators < ActiveRecord::Migration
+  def up
+    create_table :legislators do |t|
+      t.string :title
+      t.string :name
+      t.string :party
+      t.string :state
+      t.string :in_office
+      t.string :gender
+      t.string :phone
+      t.string :fax
+      t.string :website
+      t.string :webform
+      t.string :birthdate
+      t.string :twitter_id
+    end
+  end
+
+  def down
+    drop_table :legislators
+  end
+end
